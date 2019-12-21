2028d29bc55e10f73c353fd50022a0f71af547bf
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-21 22:39:10 +0100

    feat: show map and zoom buttons

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width='100vw'
+        height='calc(100vh - 64px)'
+        mapStyle='mapbox://styles/mapbox/satellite-v9'
+        {...viewport}
+        onViewportChange={setViewport}
+        mapboxApiAccessToken='pk.eyJ1IjoibmlkaGFsYnQiLCJhIjoiY2s0ZzNkM25tMHNqcjNsbzM1Y2FteXF3NCJ9._RgBFukWRU5uQtQT_lT4Rg'>
+
+        <div className={classes.navigationControl}>
+          <NavigationControl onViewportChange={setViewport}/>
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from '../components/Header';
+import Map from '../components/Map';
 import withRoot from "../withRoot";
 
 const App = () => {
   return <div>
     <Header/>
+    <Map/>
   </div>;
 };
 
