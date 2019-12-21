a2513ddde1106ec84fef1145f794efe883110e0f
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-21 13:30:22 +0100

    feat: connect to atlas mongodb

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('DB connect'))
+  .catch(() => console.error('A77lili'));
 
 const server = new ApolloServer({
   typeDefs,
