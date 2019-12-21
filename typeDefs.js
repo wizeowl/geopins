8b7b171f01af7b5ea5366407ea651c311ee65169
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-21 13:53:22 +0100

    feat: define user, pin, and comment models

--- typeDefs.js
+++ typeDefs.js
@@ -3,9 +3,27 @@ const { gql } = require('apollo-server');
 module.exports = gql`
   type User {
     _id: ID
-    name: String!
-    email: String!
-    picture: String!
+    name: String
+    email: String
+    picture: String
+  }
+
+  type Pin {
+    _id: String
+    createdAt: String
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    author: User
+    comments: [Comment]
+  }
+
+  type Comment {
+    text: String
+    createAt: String
+    author: User
   }
 
   type Query {
