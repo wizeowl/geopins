5d7608183b7fdcc9af48b56d90085abc9cb090fe
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-24 21:24:53 +0100

    feat: set server port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ const server = new ApolloServer({
   }
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT }).then(({ url }) => {
   console.log('Server listening on', url);
 });
